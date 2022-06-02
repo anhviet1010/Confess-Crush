a5bf3ac1a8fc84443081a3dca1b189c0f4f93680
anhviet1010 <105041807+anhviet1010@users.noreply.github.com>
2022-06-02 17:39:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "tre non",
-    introTitle: 'tre non',
+    titleWeb: "Việt dz",
+    introTitle: 'việt dz',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
